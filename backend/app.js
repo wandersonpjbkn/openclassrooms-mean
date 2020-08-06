8a2292266a375395f757a58db9d307bb6a665272
Wanderson PJ <wandersonpj_bkn@hotmail.com>
2020-08-06 01:51:23 -0300

    Complete the CRUD with update and delete

--- backend/app.js
+++ backend/app.js
@@ -73,6 +73,44 @@ app.get('/api/stuff/:id', (req, res, next) => {
     })
 })
 
+app.put('/api/stuff/:id', (req, res, next) => {
+  const thing = new Thing({
+    _id: req.params.id,
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+  })
+
+  Thing
+    .updateOne({ _id: req.params.id }, thing)
+    .then(things => {
+      res
+        .status(200)
+        .json(things)
+    })
+    .catch(err => {
+      res
+        .status(400)
+        .json({ err: err })
+    })
+})
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+  .then(() => {
+    res
+      .status(200)
+      .json({ message: 'Thind deleted successfully' })
+  })
+  .catch(err => {
+    res
+      .status(400)
+      .json({ err: err })
+  })
+})
+
 app.use('/api/stuff', (req, res, next) => {
   Thing
     .find()
