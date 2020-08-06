32302968ef8d0c51f643c24f891e1c0a4c6020f6
Wanderson PJ <wandersonpj_bkn@hotmail.com>
2020-08-06 00:35:22 -0300

    Create a POST route

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,23 @@
 // external
 const express = require('express')
+const { json } = require('body-parser')
 
 const app = express()
+
+app.use(json())
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
   next()
+})
+
+app.post('/api/stuff', (req, res, next) => {
+  console.log(req.body)
+  res
+    .status(201)
+    .json({ message: 'Thing created successfully' })
 })
 
 app.use('/api/stuff', (req, res, next) => {
