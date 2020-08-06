043ffc847bc940b90fb188d3cb21626b294b2259
Wanderson PJ <wandersonpj_bkn@hotmail.com>
2020-08-06 00:16:53 -0300

    Create a GET route

--- backend/app.js
+++ backend/app.js
@@ -4,22 +4,43 @@ const express = require('express')
 const app = express()
 
 app.use((req, res, next) => {
-  console.log('Request received!')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
   next()
 })
 
-app.use((req, res, next) => {
-  res.status(201)
-  next()
-})
+app.use('/api/stuff', (req, res, next) => {
+  const stuff = [
+    {
+      _id: 'quwiquwiuq',
+      title: 'My first thing',
+      description: 'All the info about my first thing',
+      imageUrl: 'https://source.unsplash.com/weekly?tech',
+      price: 8500,
+      userId: 'qoeiqoewi'
+    },
+    {
+      _id: 'zxcmzncmzn',
+      title: 'My second thing',
+      description: 'All the info about my second thing',
+      imageUrl: 'https://source.unsplash.com/weekly?trip',
+      price: 4700,
+      userId: 'lgkjglhkj'
+    },
+    {
+      _id: 'falkfnakl',
+      title: 'My third thing',
+      description: 'All the info about my third thing',
+      imageUrl: 'https://source.unsplash.com/weekly?city',
+      price: 8900,
+      userId: 'aldaasdad'
+    }
+  ]
 
-app.use((req, res, next) => {
-  res.json({ message: 'Your request was successful!' })
-  next()
-})
-
-app.use((req, res, next) => {
-  console.log('Response sent successfully')
+  res
+    .status(200)
+    .json(stuff)
 })
 
 module.exports = app
