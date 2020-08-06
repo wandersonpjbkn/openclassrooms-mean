683e9da29cac2394601ca60730802a1d4e1d092d
Wanderson PJ <wandersonpj_bkn@hotmail.com>
2020-08-06 00:00:47 -0300

    Create an Express app

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
+// external
 const http = require('http')
 
-const server = http.createServer((req, res) => {
-  res.end('This is my 10th server response')
-})
+// internal
+const app = require('./app')
+
+app.set('port', process.env.PORT || 3000)
+
+const server = http.createServer(app)
 
 server.listen(process.env.PORT || 3000)
