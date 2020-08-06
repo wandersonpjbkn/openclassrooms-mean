581ae7bc82594a5dc3cf6affaed37e72cc2e0709
Wanderson PJ <wandersonpj_bkn@hotmail.com>
2020-08-06 00:08:09 -0300

    Improve server.js

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,52 @@ const http = require('http')
 // internal
 const app = require('./app')
 
-app.set('port', process.env.PORT || 3000)
+const normalizePort = val => {
+  const port = parseInt(val, 10)
+
+  if (isNaN(port)) return val
+
+  if (port >= 0) return port
+
+  return false
+}
+
+const port = normalizePort(process.env.PORT || '3000')
+
+app.set('port', port)
+
+const errorHandler = err => {
+  if (err.syscall !== 'listen') throw err
+
+  const address = server.address()
+  const bind = typeof address === 'string'
+    ? `pipe ${address}`
+    : `port ${port}`
+
+  switch (err.code) {
+    case 'EACCES':
+      console.log(`${bind} requires elevated privileges`)
+      process.exit(1)
+
+    case 'EADDRINUSE':
+      console.log(`${bind} is already in use`)
+      process.exit(1)
+  
+    default:
+      throw err
+  }
+}
 
 const server = http.createServer(app)
 
-server.listen(process.env.PORT || 3000)
+server.on('error', errorHandler)
+server.on('listening', () => {
+  const address = server.address()
+  const bind = typeof address === 'string'
+    ? `pipe ${address}`
+    : `port ${port}`
+  
+  console.log(`Listening on ${bind}`)
+})
+
+server.listen(port)
