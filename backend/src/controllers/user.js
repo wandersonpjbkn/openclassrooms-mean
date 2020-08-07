4bca147cd9ba8a6c12d8c8528451a5abbf1316b7
Wanderson PJ <wandersonpj_bkn@hotmail.com>
2020-08-06 21:48:49 -0300

    Check a user's credentials

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -26,11 +26,43 @@ module.exports = {
               .status(500)
               .json({ err: err })
           })
-          .finally(() => { console.log('user added') })
+          .finally(() => { console.log('signup finished') })
       })
   },
 
   login: (req, res) => {
-    //
+    User
+      .findOne({ email: req.body.email })
+      .then(user => {
+        if (!user) return res.status(401).json({
+          err: new Error('User not found!')
+        })
+
+        bcrypt
+          .compare(req.body.password, user.password)
+          .then(valid => {
+            if (!valid) return res.status(401).json({
+              err: new Error('Invalid password!')
+            })
+
+            res
+              .status(200)
+              .json({
+                userID: user._id,
+                token: 'token'
+              })
+          })
+          .catch(err => {
+            res
+              .status(500)
+              .json({ err: err })
+          })
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ err: err })
+      })
+      .finally(() => { console.log('login finished') })
   }
 }
